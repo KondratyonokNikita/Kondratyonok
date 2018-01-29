4e47066bc777f582823efde19f1c6d8c49b70b40
KondratyonokNikita <nkondr2006@rambler.ru>
2018-01-29 16:08:31 +0300

    added radiobuttons checking

--- app/src/main/java/com/kondratyonok/kondratyonok/LayoutActivity.java
+++ app/src/main/java/com/kondratyonok/kondratyonok/LayoutActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
 
 public class LayoutActivity extends AppCompatActivity {
 
@@ -18,5 +19,21 @@ public class LayoutActivity extends AppCompatActivity {
         intent.setClass(view.getContext(), LauncherActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    public void setStandardLayout(View view) {
+        RadioButton standardRB = findViewById(R.id.standard_radio_button);
+        RadioButton denseRB = findViewById(R.id.dense_radio_button);
+        boolean isStandardLayout = true;
+        standardRB.setChecked(isStandardLayout);
+        denseRB.setChecked(!isStandardLayout);
+    }
+
+    public void setDenseLayout(View view) {
+        RadioButton standardRB = findViewById(R.id.standard_radio_button);
+        RadioButton denseRB = findViewById(R.id.dense_radio_button);
+        boolean isStandardLayout = false;
+        standardRB.setChecked(isStandardLayout);
+        denseRB.setChecked(!isStandardLayout);
     }
 }
--- app/src/main/java/com/kondratyonok/kondratyonok/ThemeActivity.java
+++ app/src/main/java/com/kondratyonok/kondratyonok/ThemeActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.RadioButton;
 
 public class ThemeActivity extends AppCompatActivity {
 
@@ -19,5 +20,21 @@ public class ThemeActivity extends AppCompatActivity {
         intent.setClass(view.getContext(), LayoutActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    public void setLightTheme(View view) {
+        RadioButton lightRB = findViewById(R.id.light_radio_button);
+        RadioButton darkRB = findViewById(R.id.dark_radio_button);
+        boolean isLightTheme = true;
+        lightRB.setChecked(isLightTheme);
+        darkRB.setChecked(!isLightTheme);
+    }
+
+    public void setDarkTheme(View view) {
+        RadioButton lightRB = findViewById(R.id.light_radio_button);
+        RadioButton darkRB = findViewById(R.id.dark_radio_button);
+        boolean isLightTheme = false;
+        lightRB.setChecked(isLightTheme);
+        darkRB.setChecked(!isLightTheme);
     }
 }
